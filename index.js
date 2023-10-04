15e40abf32f132b5aa217a08746f982dece0e84e
Tuvi9 <kaspar.pavel@voco.ee>
2023-10-04 15:04:09 +0300

    Add template system to project

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const app = express()
 
+//HTML 'views' Files
+const path = require('path')
+app.set('view engine', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'))
+
+const parser = require('body-parser')
+let encodeUrl = parser.urlencoded({extended: true});
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.render('validate_form.ejs')
   })
+app.post('/validate', encodeUrl, (req, res) => {
+  console.log(req.body)
+  //console.log(req.body.Vastus)
+})
 
 app.listen(3000, () => {
     console.log(`Example app is started at http://localhost:3000`)
